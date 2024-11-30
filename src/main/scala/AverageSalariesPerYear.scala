593c77735e1425be576e758c975ff5d48098f020
Noam <simonoam@gmail.com>
2024-11-30 18:48:44 +0200

    fix scala version

--- src/main/scala/AverageSalariesPerYear.scala
+++ src/main/scala/AverageSalariesPerYear.scala
@@ -1,41 +1,44 @@
+import com.mysql.cj.jdbc.Driver
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 
-object AverageSalariesPerYear extends App {
-  val spark = SparkSession.builder()
-    .appName("AverageSalariesPerYear")
-    .master("local")
-    .getOrCreate()
-  val salariesDF = spark.read
-    .format("jdbc")
-    .option("url", "jdbc:mysql://localhost:3306/lahman2016?useUnicode=true&characterEncoding=UTF-8&autoReconnect=true")
-    .option("driver", "com.mysql.cj.jdbc.Driver")
-    .option("dbtable", "lahman2016.salaries")
-    .option("user", "root")
-    .option("password", "1111")
-    .load()
-  val fieldingDF = spark.read
-    .format("jdbc")
-    .option("url", "jdbc:mysql://localhost:3306/lahman2016?useUnicode=true&characterEncoding=UTF-8&autoReconnect=true")
-    .option("driver", "com.mysql.cj.jdbc.Driver")
-    .option("dbtable", "lahman2016.fielding")
-    .option("user", "root")
-    .option("password", "1111")
-    .load()
-  val pitchingDF = spark.read
-    .format("jdbc")
-    .option("url", "jdbc:mysql://localhost:3306/lahman2016?useUnicode=true&characterEncoding=UTF-8&autoReconnect=true")
-    .option("driver", "com.mysql.cj.jdbc.Driver")
-    .option("dbtable", "lahman2016.pitching")
-    .option("user", "root")
-    .option("password", "1111")
-    .load()
-  val pitcherSalariesDF = salariesDF.join(pitchingDF, Seq("playerID", "yearID"))
-    .groupBy(col("yearID")).avg("salary").select(col("yearID").alias("year"), col("avg(salary)").alias("Pitching"))
-  val infielderSalariesDF = salariesDF.join(fieldingDF, Seq("playerID", "yearID"))
-    .groupBy(col("yearID")).avg("salary").select(col("yearID").alias("year"), col("avg(salary)").alias("Pitching"))
+object AverageSalariesPerYear  {
+  // Main Method
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName("AverageSalariesPerYear")
+      .master("local")
+      .getOrCreate()
+    val salariesDF = spark.read
+      .format("jdbc")
+      .option("url", "jdbc:mysql://localhost:3306/lahman2016?useUnicode=true&characterEncoding=UTF-8&autoReconnect=true")
+      .option("driver", "com.mysql.cj.jdbc.Driver")
+      .option("dbtable", "lahman2016.salaries")
+      .option("user", "root")
+      .option("password", "1111")
+      .load()
+    val fieldingDF = spark.read
+      .format("jdbc")
+      .option("url", "jdbc:mysql://localhost:3306/lahman2016?useUnicode=true&characterEncoding=UTF-8&autoReconnect=true")
+      .option("driver", "com.mysql.cj.jdbc.Driver")
+      .option("dbtable", "lahman2016.fielding")
+      .option("user", "root")
+      .option("password", "1111")
+      .load()
+    val pitchingDF = spark.read
+      .format("jdbc")
+      .option("url", "jdbc:mysql://localhost:3306/lahman2016?useUnicode=true&characterEncoding=UTF-8&autoReconnect=true")
+      .option("driver", "com.mysql.cj.jdbc.Driver")
+      .option("dbtable", "lahman2016.pitching")
+      .option("user", "root")
+      .option("password", "1111")
+      .load()
+    val pitcherSalariesDF = salariesDF.join(pitchingDF, Seq("playerID", "yearID"))
+      .groupBy(col("yearID")).avg("salary").select(col("yearID").alias("year"), col("avg(salary)").alias("Pitching"))
+    val infielderSalariesDF = salariesDF.join(fieldingDF, Seq("playerID", "yearID"))
+      .groupBy(col("yearID")).avg("salary").select(col("yearID").alias("year"), col("avg(salary)").alias("Pitching"))
 
-  pitcherSalariesDF.join(infielderSalariesDF, "year").show()
-  infielderSalariesDF.show()
-
+    pitcherSalariesDF.join(infielderSalariesDF, "year").show()
+    infielderSalariesDF.show()
+  }
 }
